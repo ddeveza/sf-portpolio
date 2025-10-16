7459d63467a92c78c34b16f36faaf0b945f638b8
Dennis Deveza <ddeveza@ymail.com>
2025-10-16 14:31:38 +0800

    added hero page

---
+++ force-app/main/default/lwc/hero/hero.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images';
+export default class Hero extends LightningElement {
+  imageUrl = `${IMAGES}/hero.png`;
+}
